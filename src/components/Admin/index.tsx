c84b0c82336f4b1209e13555fd71c326907374d3
mananjaykumar <sinharishab100@gmail.com>
2023-09-18 16:14:28 +0530

    fixes

--- src/components/Admin/index.tsx
+++ src/components/Admin/index.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import AdminLogin from "./AdminAuth/AdminLogin";
-import Sidebar from "./AdminLayout/Sidebar";
+import { Navigate } from "react-router-dom";
+import * as routes from "../../routes/constants";
 
 const Admin = () => {
   const { userData } = useSelector((state: any) => state.auth);
-  if (userData?.role?.includes("admin"))
-    return (
-      <div>
-        <Sidebar>kjfldjlkfjkfj</Sidebar>
-      </div>
-    );
-  else return <AdminLogin />;
+  if (userData?.role?.includes("admin")) {
+    return <Navigate to={routes.R_UPLOAD_BANNER} />;
+  } else return <AdminLogin />;
 };
 
 export default Admin;
