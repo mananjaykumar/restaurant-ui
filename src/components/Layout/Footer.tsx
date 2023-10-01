5fac264132b9a21aca113ce3e3c363d7aa3838bc
mananjaykumar <sinharishab100@gmail.com>
2023-10-02 03:58:11 +0530

    some fixes

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
       sx={{
         backgroundColor: "rgb(2, 6, 12)",
         color: "white",
-        padding: "150px 10px",
+        padding: "100px 10px",
       }}
     >
       <Stack
