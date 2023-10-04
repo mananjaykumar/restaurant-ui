8687e5f48aabdca8138920a5ccbb3349452a837d
mananjaykumar <sinharishab100@gmail.com>
2023-10-04 20:16:28 +0530

    fixed@v2

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -170,13 +170,13 @@ function Item(props: any) {
             height: {
               xs: "220px",
               sm: "250px",
-              marginRight: {
-                xs: "auto",
-                sm: "auto",
-                md: "auto",
-                lg: "auto",
-                xl: 0,
-              },
+            },
+            marginRight: {
+              xs: "auto",
+              sm: "auto",
+              md: "auto",
+              lg: "auto",
+              xl: 0,
             },
           }}
           // src={props.item.image}
